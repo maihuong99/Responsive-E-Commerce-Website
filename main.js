5c42533210f7fefe5acf171a2cc60506f67deb19
maihuong99 <62054925+maihuong99@users.noreply.github.com>
2021-07-26 19:11:31 +0700

    complete home section and arrival section

--- main.js
+++ main.js
@@ -15,5 +15,14 @@ $(document).ready(function(){
         }
     })
 
+    $('.home-slider').owlCarousel({
+        items: 1,
+        nav: true,
+        dots:false,
+        autoplay: true,
+        autoplayTimeout: 8000,
+        loop: true,
+    })
+
     
 });
