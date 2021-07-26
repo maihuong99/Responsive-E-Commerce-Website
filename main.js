80863efedcf325626496dd69d0942c03c4b6dfc6
maihuong99 <62054925+maihuong99@users.noreply.github.com>
2021-07-26 15:05:50 +0700

    complete header section

---
+++ main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('#menu').on("click",function(){
+        $(this).toggleClass("fa-times");
+        $(".navbar").toggleClass("nav-toggle");
+    })
+    
+    $(window).on('scroll load', function(){
+        $("#menu").removeClass("fa-times");
+        $(".navbar").removeClass("nav-toggle");
+
+        if($(window).scrollTop() >68){
+            $('header .header-2').addClass('header-active');
+        }else{
+            $('header .header-2').removeClass('header-active');
+        }
+    })
+
+    
+});
