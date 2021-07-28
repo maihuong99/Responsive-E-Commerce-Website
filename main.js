ae0187ddeb2c2572241bffe97746cc313af60c1d
maihuong99 <62054925+maihuong99@users.noreply.github.com>
2021-07-28 18:18:50 +0700

    complete featured section, gallery section and scroll spy

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ $(document).ready(function(){
         $(this).toggleClass("fa-times");
         $(".navbar").toggleClass("nav-toggle");
     })
+
+    $('.navbar ul li a').click(function(){
+        $(this).addClass("active").siblings().removeClass("active");
+    })
     
     $(window).on('scroll load', function(){
         $("#menu").removeClass("fa-times");
@@ -13,6 +17,18 @@ $(document).ready(function(){
         }else{
             $('header .header-2').removeClass('header-active');
         }
+
+        $('section').each(function(){
+            let height = $(this).height();
+            let offset = $(this).offset().top-200;
+            let top = $(window).scrollTop();
+            let id =$(this).attr('id');
+
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`[href="#${id}"]`).addClass('active');
+            }
+        })
     })
 
     $('.home-slider').owlCarousel({
@@ -22,6 +38,26 @@ $(document).ready(function(){
         autoplay: true,
         autoplayTimeout: 8000,
         loop: true,
+    })
+
+    $('.small-image img').click(function(){
+        $(this).addClass('img-active').siblings().removeClass('img-active');
+
+        let image = $(this).attr('src');
+        $('.big-image img').attr('src', image);
+    })
+
+    $('.gallery .btn').click(function(){
+        let filter = $(this).attr('data-filter');
+        if(filter =='all'){
+            $('.gallery .box').show(400);
+        }
+        else{
+            $('.gallery .box').not('.'+filter).hide(200);
+            $('.gallery .box').filter('.'+filter).show(400);
+        }
+
+        $(this).addClass('btn-active').siblings().removeClass('btn-active');
     })
 
     
